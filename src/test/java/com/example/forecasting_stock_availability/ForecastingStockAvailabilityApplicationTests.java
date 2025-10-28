7f2716bdad76dbafbd8949b2988011c16d41bf79
ZdenekVavricka <zdenda.vavricka@seznam.cz>
2025-10-28 13:23:54 +0100

    first commit

---
+++ src/test/java/com/example/forecasting_stock_availability/ForecastingStockAvailabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forecasting_stock_availability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForecastingStockAvailabilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
