2da0c9202ebfddbe0fded4b240a469af3138fcb5
ZdenekVavricka <zdenda.vavricka@seznam.cz>
2025-11-09 19:58:23 +0100

    add Prediction Endpoint

---
+++ src/main/java/com/example/forecasting_stock_availability/prediction/Prediction.java
@@ -0,0 +1,22 @@
+package com.example.forecasting_stock_availability.prediction;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@Component
+@RestController
+public class Prediction {
+
+    //date, shop, item
+    @GetMapping("/predict/{date}/{shop}/{item}")
+    public static String predictDate(@PathVariable String date, @PathVariable String shop, @PathVariable String item) {
+        //validate inputs
+
+        
+        //TODO
+        return "TODO = " + date + " " + shop + " " + item;
+    }
+
+}
